2af21b648368ba5a73afdf207fbf27dea9f01800
alex_rb <alexisjrb2908@gmail.com>
2023-09-02 16:32:10 -0300

    proyecto

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,9 +2,6 @@ import ItemList from "../ItemList/ItemList";
 import { useGetProducts } from "../../hooks/useGetProducts";
 import { useParams } from "react-router-dom";
 
-import { getDocs, collection, query, where } from "firebase/firestore";
-import { db } from "../../config/firebase";
-
 const ItemListContainer = () => {
   const { categoryId } = useParams();
   const { products } = useGetProducts(categoryId);
